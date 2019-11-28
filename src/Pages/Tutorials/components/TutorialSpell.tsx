436a5702f5e3c8756c15a4b0cce605a69a8bb07a
ozan <ozanaraz91@gmail.com>
2019-11-28 23:33:00 +0100

    creates tutorial spell

---
+++ src/Pages/Tutorials/components/TutorialSpell.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from 'react';
+import './TutorialSpell.css';
+
+const TutorialSpell: FunctionComponent<Spell> = (props: Spell) => {
+    const { description, image, name } = props;
+    return (
+        <div className="tutorial-spell">
+            <div className="tutorial-spell-item-header">
+                <span>
+                    <img
+                        className="tutorial-spell-item-image"
+                        src={`http://ddragon.leagueoflegends.com/cdn/9.22.1/img/${image.group}/${image.full}`}
+                    />
+                </span>
+                <span className="tutorial-spell-item-name">{name}</span>
+            </div>
+            {description}
+        </div>
+    );
+};
+
+export { TutorialSpell };
---
+++ src/Pages/Tutorials/components/index.ts
@@ -0,0 +1,1 @@
+export { TutorialSpell } from './TutorialSpell';
