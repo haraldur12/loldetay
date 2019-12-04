85aa6fda9d5fd25d18293e206dd365c8ec1739f9
ozan <ozanaraz91@gmail.com>
2019-12-04 21:47:53 +0100

    use https for calls

--- src/Pages/Tutorials/utilities/getSummonerSpells.ts
+++ src/Pages/Tutorials/utilities/getSummonerSpells.ts
@@ -1,6 +1,6 @@
 export const getSummonerSpells = async (): Promise<any> => {
     return new Promise(resolve => {
-        fetch(`http://ddragon.leagueoflegends.com/cdn/9.23.1/data/tr_TR/summoner.json`)
+        fetch(`https://ddragon.leagueoflegends.com/cdn/9.23.1/data/tr_TR/summoner.json`)
             .then(response => response.json())
             .then(body => {
                 resolve(body.data);
